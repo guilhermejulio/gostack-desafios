a05d087da82f7909870f6d315ffea31c9809d7ff
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-03-02 09:11:20 -0300

    Add. Challenge 2

---
+++ desafio-2/conceitos-nodejs/src/app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4: uuid, validate: isUuid } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+function validateRepositoryId(request, response, next){
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(repository == null){
+    return response.status(400).json({error: 'Repository not found.'});
+  }
+
+  return next();
+}
+
+app.get("/repositories", (request, response) => {
+  response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+ 
+  const repository = repositories.find(repository => repository.id === id);
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  // if(repository == null){
+  //   return response.status(400).json({error: 'Repository not found.'});
+  // }
+
+  repository.likes += 1;
+
+  return response.json(repository);
+});
+
+module.exports = app;
